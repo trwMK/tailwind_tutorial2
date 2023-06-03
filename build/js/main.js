265553fa19f09d532ec019a42ee2f754d3a19248
Mike Koenders <mike.koenders29@gmail.com>
2023-06-03 15:58:42 +0200

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,16 @@
+const initApp = () => {
+  const hamburgerBtn = document.getElementById("hamburger-button");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+    hamburgerBtn.classList.toggle("toggle-btn");
+  };
+
+  hamburgerBtn.addEventListener("click", toggleMenu);
+  mobileMenu.addEventListener("click", toggleMenu);
+};
+
+//We have to wait until the DOM is loaded, otherwise the button we try to select will cause errors, because we are not able to select them before
+document.addEventListener("DOMContentLoaded", initApp);
